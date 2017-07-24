4c8ac3325a0eaa00208d9df3e40d2cfa28d5ac8b
rivermont <william.11bennett@gmail.com>
2017-07-23 21:52:07 -0400

    Added 003.py

---
+++ solutions/003.py
@@ -0,0 +1,28 @@
+# Project Euler Problem 2
+# Even Fibonacci numbers
+
+# Copyright (c) 2017 - Will Bennett
+# Created under the MIT License
+
+
+def get_primes(n):
+	'''
+	Returns a list of all the prime numbers below n.
+	'''
+	i = 1
+	s = n**0.5 #Square root of n
+	a = list(range(1, (n + 1)))
+	p = []
+	while i < s:
+		i += 1
+		print('i: ' + str(i))
+		p.append(i)
+		t = a[i**2-1::i] #A list of all numbers in a, starting at i^2, in increments of i
+		a = set(a)
+		for x in t:
+			print(x)
+			a.remove(x)
+		a = sorted(list(a))
+	return a, p
+
+print(get_primes(100))
